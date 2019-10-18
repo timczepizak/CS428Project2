0cd02c8910813bd7d689e582b84ef121fc774a76
timczepizak <tim_czepizak@hotmail.com>
2019-10-17 21:25:10 -0500

    commit project 2 app

---
+++ Assets/AudioChangeSCript.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioChangeSCript : MonoBehaviour
+{
+    private int key = 1;
+    public AudioSource audio;
+
+    public void ChangeAudio()
+    {
+        if (key == 1)
+        {
+            audio.pitch = 3f;
+            key = 2;
+        }
+        else if (key == 2)
+        {
+            audio.pitch = 1f;
+            key = 0;
+        }
+        else if (key == 0)
+        {
+            audio.pitch = 1f;
+            key = 1;
+        }
+
+    }
+}
---
+++ Assets/GetRidOfCeiling.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GetRidOfCeiling : MonoBehaviour
+{
+    public GameObject ceiling;
+    private int key = 1;
+    
+
+   public void GetRid()
+    {
+        if(key == 1)
+        {
+            key = 2;
+        }
+        else if (key == 2)
+        {
+            ceiling.SetActive(false);
+            key = 0;
+        }
+        else if (key == 0)
+        {
+            ceiling.SetActive(true);
+            key = 1;
+        }
+
+    }
+
+
+}
